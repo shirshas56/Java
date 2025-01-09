40b075d7b5fcda7949177569031165a6a6eeb15b
SHIRSHA SAMANTA <145319422+shirshas56@users.noreply.github.com>
2025-01-09 22:43:09 +0530

    Add files via upload

---
+++ reverse_num_pyra.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class reverse_num_pyra {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter the no. of rows: ");
+        int n = sc.nextInt(); 
+        for (int i = 1; i <= n; i++){
+            for (int j = n; j >= i; j--) {
+                System.out.print(j);
+            }
+            System.out.println(); 
+        }
+
+        sc.close(); 
+    }
+}
