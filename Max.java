8c776af5be4343861cc4dfbc16b7468d91fc72d3
SHIRSHA SAMANTA <145319422+shirshas56@users.noreply.github.com>
2025-01-16 21:01:38 +0530

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Max {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+        int max = num1;
+        if (num2 > max) {
+            max = num2;
+        }
+        if (num3 > max) {
+            max = num3;
+        }
+        System.out.println("The maximum of the three numbers is: " + max);
+        sc.close();
+    }
+}
